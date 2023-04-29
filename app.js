ae109f50ba4042b5ae15bbfca967688033c69562
Harsha-ksr <harsha.kshirsagar11@gmail.com>
2023-04-29 13:01:32 -0400

    second commit

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ var mime = {
     js: 'application/javascript'
 };
 const Destinations = [ 
-    { PlaceName: 'Osaka', Country:'Japan' , Description:'Japan is an island country in East Asia', FamousFor: 'famous for anime', BestTimeToVisit:'summer', Rank:3, NoOfVisitor:12345},
-    { PlaceName: 'Seoul', Country:'South Korea' , Description:'City in South Korea, an East Asian nation on the southern half of the Korean Peninsula', FamousFor: 'famous for K-pop and K-Drama', BestTimeToVisit:'summer', Rank:4, NoOfVisitor:32345},
-    { PlaceName: 'Reykjavík', Country:'Iceland' , Description:'Reykjavík is the capital and largest city of Iceland. ', FamousFor: 'famous for  unique architecture, wild nightlife', BestTimeToVisit:'summer and Winter', Rank:2, NoOfVisitor:12345},
-    { PlaceName: 'Alaska', Country:'United States of America' , Description:'Alaska is a U.S. state on the northwest extremity of North America.', FamousFor: 'famous for nature, scenery, northern lights', BestTimeToVisit:'summer', Rank:1, NoOfVisitor:782345},
-    { PlaceName: 'Havana', Country:'Cuba' , Description:'Havana is Cuba’s capital city.', FamousFor: 'famous for Spanish colonial architecture', BestTimeToVisit:'Anytime', Rank:5, NoOfVisitor:224345}
+    { PlaceName: 'Osaka', Country:'Japan' , Description:'Japan is an island country in East Asia', FamousFor: 'famous for anime', BestTimeToVisit:'summer', Rank:1, NoOfVisitor:12345},
+    { PlaceName: 'Seoul', Country:'South Korea' , Description:'City in South Korea, an East Asian nation on the southern half of the Korean Peninsula', FamousFor: 'famous for K-pop and K-Drama', BestTimeToVisit:'summer', Rank:2, NoOfVisitor:32345},
+    { PlaceName: 'Reykjavík', Country:'Iceland' , Description:'Reykjavík is the capital and largest city of Iceland. ', FamousFor: 'famous for  unique architecture, wild nightlife', BestTimeToVisit:'summer and Winter', Rank:3, NoOfVisitor:12345},
+    { PlaceName: 'Alaska', Country:'United States of America' , Description:'Alaska is a U.S. state on the northwest extremity of North America.', FamousFor: 'famous for nature, scenery, northern lights', BestTimeToVisit:'summer', Rank:4, NoOfVisitor:782345},
+    { PlaceName: 'Havana', Country:'Cuba' , Description:'Havana is Cuba’s capital city.', FamousFor: 'famous for Spanish colonial architecture', BestTimeToVisit:'Anytime', Rank:5, NoOfVisitor:224345},
+    { PlaceName: 'paris', Country:'France' , Description:'Paris, France’s capital, is a major European city and a global center for art', FamousFor: 'famous for Art and Museums', BestTimeToVisit:'summer', Rank:6, NoOfVisitor:452345},
+    { PlaceName: 'kerala', Country:'India' , Description:'Kerala, a state on India’s tropical Malabar Coast, has nearly 600km of Arabian Sea shoreline.', FamousFor: ' palm-lined beaches and backwaters,', BestTimeToVisit:'summer', Rank:7, NoOfVisitor:32345}
+    
 ];
 
 const server = http.createServer(function(req, res) {
