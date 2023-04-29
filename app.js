fe7b6062c278796c9070bb93741629a6120505b3
Harsha-ksr <harsha.kshirsagar11@gmail.com>
2023-04-29 11:56:16 -0400

    FirstCommit

---
+++ app.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+var dir = path.join(__dirname, 'public');
+var mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
+const Destinations = [ 
+    { PlaceName: 'Osaka', Country:'Japan' , Description:'Japan is an island country in East Asia', FamousFor: 'famous for anime', BestTimeToVisit:'summer', Rank:3, NoOfVisitor:12345},
+    { PlaceName: 'Seoul', Country:'South Korea' , Description:'City in South Korea, an East Asian nation on the southern half of the Korean Peninsula', FamousFor: 'famous for K-pop and K-Drama', BestTimeToVisit:'summer', Rank:4, NoOfVisitor:32345},
+    { PlaceName: 'Reykjavík', Country:'Iceland' , Description:'Reykjavík is the capital and largest city of Iceland. ', FamousFor: 'famous for  unique architecture, wild nightlife', BestTimeToVisit:'summer and Winter', Rank:2, NoOfVisitor:12345},
+    { PlaceName: 'Alaska', Country:'United States of America' , Description:'Alaska is a U.S. state on the northwest extremity of North America.', FamousFor: 'famous for nature, scenery, northern lights', BestTimeToVisit:'summer', Rank:1, NoOfVisitor:782345},
+    { PlaceName: 'Havana', Country:'Cuba' , Description:'Havana is Cuba’s capital city.', FamousFor: 'famous for Spanish colonial architecture', BestTimeToVisit:'Anytime', Rank:5, NoOfVisitor:224345}
+];
+
+const server = http.createServer(function(req, res) {
+    if (req.url.includes('/api')) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify(Destinations));
+    }
+    var reqpath = req.url.toString().split('?')[0]
+    var file = path.join(dir, reqpath.replace(/\/$/, '/index.html'));
+    if (file.indexOf(dir + path.sep) !== 0) {
+        res.statusCode = 403;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Forbidden');
+    }
+    var type = mime[path.extname(file).slice(1)] || 'text/plain';
+    var s = fs.createReadStream(file);
+    s.on('open', function () {
+        res.setHeader('Content-Type', type);
+        s.pipe(res);
+    });
+    s.on('error', function () {
+        res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 404;
+        res.end('Not found');
+    });
+});
+
+const PORT = process.env.PORT || 5555;
+server.listen(PORT, function () {
+    console.log(`Server running on port ${PORT}`);
+});
+
+//server.listen(PORT, () => console.log(`Server running on port ${PORT}`));*/
